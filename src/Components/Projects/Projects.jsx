a273e413afe192fa8e7baea7279ee05235fb9a7f
mrdeerek <kunalraj845438@gmail.com>
2025-08-29 12:07:19 +0530

    Refactor code structure for improved readability and maintainability

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,19 +2,28 @@ import React from 'react'
 import './Projects.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import project_data from '../../assets/Project_data'
+import arrow_icon from '../../assets/arrow_icon.svg'
+
 const Projects = () => {
   return (
     <div className='projects'>
-        <div className='projects-title'>
+              <div className="services-title">
+                <h1>Latest Project</h1>
+                <img src={theme_pattern} alt="" />
+              </div>
+        {/* <div className='projects-title'>
             <h1>My Latest Projects</h1>
             <img src={theme_pattern} alt="" />
-        </div>
-        <div className="project-container">
+        </div> */}
+        <div className="projects-container">
             {project_data.map((project,index)=>{
-
+                return <img key={index} src={project.w_img} alt="" />
             })}
         </div>
-      
+        <div className="projects-showmore">
+            <p>Show More</p>
+            <img src={ arrow_icon} alt="" />
+        </div>
     </div>
   )
 }
